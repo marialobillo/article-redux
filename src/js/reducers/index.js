f515a0a7dd05448a9fff36ef34247ad12d735ce7
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-05-12 18:41:54 +0200

    spread operator its fun

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,8 +1,19 @@
+import { ADD_ARTICLE } from '../constants/action-types';
+
 const initialState = {
 	articles: []
 };
 
 
-const rootReducer = (state = initialState, action) => state;
+const rootReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case ADD_ARTICLE:
+			//state.articles.push(action.payload);
+			//return { ...state, articles: state.articles.concat(action.payload) };
+			return {...state, articles: [...state.articles, action.payload] };
+		default:
+			return state;
+	}
+};
 
 export default rootReducer;
